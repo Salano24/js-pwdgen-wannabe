4638722843339891de3fc69df1b84b44aee4dce7
Salano24 <salernomarco48@email.com>
2022-10-05 14:54:09 +0200

    Fine esercizio

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+const anno ='22'
+/* Chiedi all’utente il suo nome,
+ */
+const nome = prompt('Inserisci il tuo nome')
+
+/* poi chiedi il suo cognome,
+ */
+const cognome = prompt('Inserisci il tuo cognome')
+
+
+/* poi chiedi il suo colore preferito
+ */
+const colore = prompt('Inserisci il tuo colore preferito')
+
+
+/* Infine scrivi sulla pagina nomecognomecolorepreferito22 */
+document.getElementById('password_consigliata').innerHTML = nome + cognome + colore + anno
